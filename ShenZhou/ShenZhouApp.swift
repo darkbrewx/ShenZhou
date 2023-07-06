e913a684939a58cbd284b9bfb437ee24c99af3e3
WENBO HU <darkbrewx@gmail.com>
2023-07-06 13:58:03 +0900

    Initial Commit

---
+++ ShenZhou/ShenZhouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShenZhouApp.swift
+//  ShenZhou
+//
+//  Created by 胡文博 on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct ShenZhouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
